fbcb2b3d1b7299c2119847a4f4e30dd5c59b02c4
psycho <moseyarthas@gmail.com>
2023-08-27 11:50:34 +0300

    userdetailsservice impl

--- backend/src/main/java/com/psycho/backend/web/security/JwtUserDetailsServiceImpl.java
+++ backend/src/main/java/com/psycho/backend/web/security/JwtUserDetailsServiceImpl.java
@@ -1,12 +1,21 @@
 package com.psycho.backend.web.security;
 
+import com.psycho.backend.service.api.UserService;
+import com.psycho.backend.web.security.factory.JwtUserDetailsFactory;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class JwtUserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserService userService;
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
+        return JwtUserDetailsFactory.build(userService.getByUsername(username));
     }
 }
