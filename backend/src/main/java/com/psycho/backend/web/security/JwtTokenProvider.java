5ef915b5e099981475a7b6e88d07703de741557b
psycho <moseyarthas@gmail.com>
2023-08-27 11:26:03 +0300

    jwttokenprovider key

--- backend/src/main/java/com/psycho/backend/web/security/JwtTokenProvider.java
+++ backend/src/main/java/com/psycho/backend/web/security/JwtTokenProvider.java
@@ -1,4 +1,23 @@
 package com.psycho.backend.web.security;
 
+import com.psycho.backend.service.api.UserService;
+import com.psycho.backend.service.props.JwtProperty;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+@RequiredArgsConstructor
 public class JwtTokenProvider {
+    private final UserService userService;
+    private final JwtProperty jwtProperty;
+    private Key key;
+
+    @PostConstruct
+    private void init() {
+        this.key = Keys.hmacShaKeyFor(jwtProperty.getSecret().getBytes());
+    }
 }
