045e41190cd8e460970baa5dd78ff8b39f4292d6
psycho <moseyarthas@gmail.com>
2023-08-27 11:38:35 +0300

    dto for jwt, + bean validation api impl in gradle.build

--- backend/src/main/java/com/psycho/backend/web/security/dto/JwtRequestDto.java
+++ backend/src/main/java/com/psycho/backend/web/security/dto/JwtRequestDto.java
@@ -1,4 +1,13 @@
 package com.psycho.backend.web.security.dto;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
 public class JwtRequestDto {
+    @NotNull(message = "Username can`t be null")
+    private String username;
+
+    @NotNull(message = "Password can`t be null")
+    private String password;
 }
