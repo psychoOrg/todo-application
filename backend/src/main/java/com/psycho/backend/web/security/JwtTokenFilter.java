04da721e0ac948080accc6050899293f07f6af10
psycho <moseyarthas@gmail.com>
2023-08-28 15:42:38 +0300

    jwt token filter

--- backend/src/main/java/com/psycho/backend/web/security/JwtTokenFilter.java
+++ backend/src/main/java/com/psycho/backend/web/security/JwtTokenFilter.java
@@ -1,4 +1,42 @@
 package com.psycho.backend.web.security;
 
-public class JwtTokenFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenFilter extends GenericFilterBean {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    @SneakyThrows
+    public void doFilter(ServletRequest request,
+                         ServletResponse response,
+                         FilterChain chain) {
+        String token = ((HttpServletRequest) request).getHeader("Authentication");
+        if (token != null && token.startsWith("Bearer ")) {
+            token = token.substring(7);
+        }
+
+        try {
+            if (token != null && jwtTokenProvider.validateAccessToken(token)) {
+                Authentication authentication = jwtTokenProvider.getAuthentication(token);
+
+                if (authentication != null) {
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }
+        } catch (Exception ignore) {
+            // Ignore
+        }
+    }
 }
