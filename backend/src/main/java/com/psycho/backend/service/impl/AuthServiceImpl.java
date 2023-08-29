bf76e1419a35dabe794571612b5aa2d5a6fc663f
psycho <moseyarthas@gmail.com>
2023-08-29 20:51:24 +0300

    auth service impl for login and register

--- backend/src/main/java/com/psycho/backend/service/impl/AuthServiceImpl.java
+++ backend/src/main/java/com/psycho/backend/service/impl/AuthServiceImpl.java
@@ -1,18 +1,21 @@
 package com.psycho.backend.service.impl;
 
+import com.psycho.backend.aop.api.Loggable;
 import com.psycho.backend.data.exception.ResourceMappingException;
 import com.psycho.backend.domain.user.Role;
 import com.psycho.backend.domain.user.User;
 import com.psycho.backend.repository.UserRepository;
 import com.psycho.backend.service.api.AuthService;
 import com.psycho.backend.service.api.UserService;
+import com.psycho.backend.web.security.JwtTokenProvider;
 import com.psycho.backend.web.security.dto.JwtRequestDto;
 import com.psycho.backend.web.security.dto.JwtResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.Set;
 
 @Service
@@ -22,11 +25,15 @@ public class AuthServiceImpl implements AuthService {
     private final PasswordEncoder passwordEncoder;
     private final UserService userService;
     private final UserRepository userRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final AuthenticationManager authenticationManager;
+
     @Override
+    @Loggable
     public User register(User user) {
 
         if (userRepository.findByUsername(user.getUsername()).isPresent()) {
-            throw new ResourceMappingException(String.format("User [%s] already exist", user.getUsername());
+            throw new ResourceMappingException(String.format("User [%s] already exist", user.getUsername()));
         }
 
         user.setPassword(passwordEncoder.encode(user.getPassword()));
@@ -45,6 +52,18 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public JwtResponseDto login(JwtRequestDto jwtRequestDto) {
-        return null;
+        JwtResponseDto jwtResponseDto = new JwtResponseDto();
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        jwtRequestDto.getUsername(), jwtRequestDto.getPassword()
+                )
+        );
+
+
+        User user = userService.getByUsername(jwtRequestDto.getUsername());
+        jwtResponseDto.setId(user.getId());
+        jwtResponseDto.setUsername(user.getUsername());
+        jwtResponseDto.setAccessToken(jwtTokenProvider.generateAccessToken());
+        return jwtResponseDto;
     }
 }
